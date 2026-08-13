7846b4759c602efb9102b877185f885b177a8300
M4ST3RCH1F3 <walterbaiarebeloneto@gmail.com>
2026-08-13 16:50:27 -0300

    v0.0.2 | Atializado o Main.py para supportar comandos no cmds/, alem de permitir os comandos via slash/prefixo

--- src/main.py
+++ src/main.py
@@ -1,23 +1,61 @@
-import discord
 import os
-from discord.ext import commands
+import sys
+import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(ROOT_DIR)
 
-load_dotenv('token.env')
+load_dotenv(os.path.join(ROOT_DIR, 'token.env'))
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-class MyClient(discord.Client):
-  async def on_ready(self):
-    print(f'Sucesso, Logado como {self.user}.')
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = MyClient(intents=intents)
+PREFIXOS = ['Om!' , 'oM!' , 'OM!' , 'Om!' , '€']
 
-if __name__ == "__main__":
-    if TOKEN:
-        client.run(TOKEN)
-    else:
-        print("Erro: Bot Token não encontrado no arquivo .env")
+def get_prefix(bot, message):
+  return commands.when_mentioned_or(*PREFIXOS)(bot, message)
+
+bot = commands.Bot(
+  command_prefix = get_prefix, 
+  intents = intents, 
+  help_command = None
+)
+
+async def load_cmds():
+  cmds_path = os.path.join(ROOT_DIR, 'cmds')
+
+  for filename in os.listdir(cmds_path):
+    if filename.endswith('.py') and filename != '__init__.py':
+      try:
+        await bot.load_extention(f"command-{filename[ : -3]}")
+        print(f"✅ Comando carregado com sucesso. | {filename}")
+      except Exception as e:
+        print(f"❎ Falha ao carregar o comando: | {filename} | {e}")
+
+@bot.event
+async def on_ready():
+  BOT_ID = bot.user.id
+  print(f"===================")
+  print(f"✅ Sucesso, bot ativo como {bot.user} | ID: {BOT_ID}")
+  print(f"===================")
+  print(f"Para desativar, use Ctrl + C.")
+  print(f"===================")
+
+  await load_cmds()
+
+  try:
+    await bot.tree.sync()
+    print(f"✅ Comandos Slash sincronizados com Sucesso.")
+    print(f"===================")
+  except Exception as e:
+    print(f"❎ Erro ao carregar o comando: {e}")
+    print(f"===================")
+  
+if TOKEN:
+    bot.run(TOKEN)
+else:
+    print("Erro: Bot Token não encontrado no arquivo token.env")
